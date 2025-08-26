4ff20d253be6047942e731efe0329a3440851253
AaravGug <amg8627@psu.edu>
2025-08-26 18:54:05 -0400

    timeout number tweak to allow more time before updating UI

--- pantry-tracker-frontend/src/components/ModifyIngredientPopup.jsx
+++ pantry-tracker-frontend/src/components/ModifyIngredientPopup.jsx
@@ -13,13 +13,13 @@ const ModifyIngredientPopup = ({ ingredientName, givenUnits, purpose, source, on
     const handleConfirm = async () => {
         if (source === "pantry") {
             ModifyIngredientEntry({ ingredientName, quantity, units });
-            await new Promise(resolve => setTimeout(resolve, 200));
+            await new Promise(resolve => setTimeout(resolve, 300));
             pantryContext.refreshPantryList();
             console.log('refreshing pantry list')
         }
         else if (source === "grocery list") {
             ModifyGroceryListEntry({ ingredientName, quantity, units });
-            await new Promise(resolve => setTimeout(resolve, 200));
+            await new Promise(resolve => setTimeout(resolve, 300));
             groceryContext.refreshGroceryList();
             console.log('refreshing grocery list');
         }
